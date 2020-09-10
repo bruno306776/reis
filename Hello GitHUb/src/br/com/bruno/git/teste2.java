73d5e67901e85c60238a9f044b523783f42f9e18
bruno306776 <70724256+bruno306776@users.noreply.github.com>
2020-09-10 20:33:33 -0300

    teste3
    teste3

---
+++ Hello GitHUb/src/br/com/bruno/git/teste2.java
@@ -0,0 +1,24 @@
+package br.com.bruno.git;
+
+public class teste2 {
+
+	public static void main(String[] args) {
+		
+		String nome;
+		int idade = 20;
+		char sexo = 'M';
+		double temperatura = 25.5;
+		nome = "Bruno Reis";
+		
+		System.out.print("Nome:" + nome);
+		System.out.print(" Idade:" + idade);
+		System.out.print(" Sexo:" + sexo);
+		System.out.print(" Temperatura:" + temperatura);
+		
+		
+		
+	
+
+	}
+
+}
